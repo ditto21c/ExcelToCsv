fba7d5172f4929f36c19e809d576f234fd5b80a8
이정권 <ditto21c@gmail.com>
2020-08-29 03:03:18 +0900

    엑셀 파일을 csv로 변경 '#' Sheet는 변경 안함

---
+++ ExcelToCsv/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExcelToCsv
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected override void OnShown(EventArgs e)
+        {
+            base.OnShown(e);
+
+            LoadExcelFiles();
+
+            Application.Exit();
+
+        }
+
+        void LoadExcelFiles()
+        {
+            CLoadExcel LoadExcel = new CLoadExcel();
+
+            System.IO.DirectoryInfo DirectoryInfo = new System.IO.DirectoryInfo(System.Environment.CurrentDirectory);
+            foreach (System.IO.FileInfo FileInfo in DirectoryInfo.GetFiles())
+            {
+                if (FileInfo.Extension == ".xlsx")
+                {
+                    LoadExcel.LoadExcel(FileInfo);
+                }
+            }
+        }
+    }
+}
---
+++ ExcelToCsv/LoadExcel.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+using Excel = Microsoft.Office.Interop.Excel;
+
+class CLoadExcel
+{
+    Excel.Application Application = new Excel.Application();
+
+    public void LoadExcel(System.IO.FileInfo FileInfo)
+    {
+        string DirectoryStr = FileInfo.Name.Split('.')[0];
+        Directory.CreateDirectory(DirectoryStr);
+
+        Excel.Workbook WorkBook = Application.Workbooks.Open(FileInfo.FullName, 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+
+        for (int i = 0; i < WorkBook.Worksheets.Count; ++i)
+        {
+            Excel.Worksheet WorkSheet = WorkBook.Worksheets.get_Item(i + 1);
+            if (WorkSheet.Name.Contains("#"))
+                continue;
+
+            FileStream fileStream = new FileStream(DirectoryStr + "/" + WorkSheet.Name + ".csv", FileMode.OpenOrCreate);
+            //StreamWriter Writer = new StreamWriter(fileStream);
+            TextWriter Writer = new StreamWriter(fileStream, System.Text.Encoding.Unicode);
+
+            Excel.Range Range = WorkSheet.UsedRange;
+
+            string CsvStr = string.Empty;
+            for(int RowIdx=1; RowIdx <= Range.Rows.Count; ++RowIdx)
+            {
+                for (int ColumnIdx = 1; ColumnIdx <= Range.Columns.Count; ++ColumnIdx)
+                {
+                    if ((Range.Cells[RowIdx, ColumnIdx] as Excel.Range).Value2 != null)
+                    { 
+                        var Value = (Range.Cells[RowIdx, ColumnIdx] as Excel.Range).Value2;
+                        CsvStr += Value;
+                        if (ColumnIdx != Range.Columns.Count)
+                        {
+                            CsvStr += ",";
+                        }
+                    }
+                    
+                }
+                CsvStr += System.Environment.NewLine;
+            }
+            Writer.Write(CsvStr);
+            Writer.Close();
+            fileStream.Close();
+        }
+
+        WorkBook.Close();
+        
+        
+    }
+}
